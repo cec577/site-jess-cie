073b0dd811484e3f6c0d223238602b1b2dd454be
cecile <cecile.henry577@gmail.com>
2020-06-28 21:10:31 +0200

    add form logic

--- src/components/devis.js
+++ src/components/devis.js
@@ -10,24 +10,37 @@ const Devis = ({ pageInfo }) => {
         Demander un devis
       </h2>
       <Container data-aos="fade-up">
-        <Form>
+        <Form
+          method="post"
+          name="devis"
+          data-netlify="true"
+          data-netlify-honeypost="bot-field"
+        >
           <Row>
             <Col md="6">
-              <Form.Control type="text" placeholder="Prénom" />
+              <Form.Control type="text" name="prenom" placeholder="Prénom" />
             </Col>
             <Col md="6">
-              <Form.Control type="text" placeholder="Nom" />
+              <Form.Control type="text" name="nom" placeholder="Nom" />
             </Col>
           </Row>
 
           <Form.Group controlId="exampleForm.ControlInput1">
             {/*<Form.Label>Email address</Form.Label>*/}
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="name@example.com"
+            />
           </Form.Group>
-          <Form.Control type="text" placeholder="numéro de téléphone" />
+          <Form.Control
+            type="text"
+            name="tel"
+            placeholder="numéro de téléphone"
+          />
 
           <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Control as="textarea" rows="3" />
+            <Form.Control as="textarea" rows="3" name="demande" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Envoyer
